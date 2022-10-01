9d5a4df0be1b09e4c25a5d6fe4ddf10ebcc0001c
Gabriel Giangi <ggiangi@shipnowinc.com>
2022-10-01 16:22:43 -0400

    vercel issue fix

--- .next/static/wDkcxfEfprU_kVfzEHM5e/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/14-6c4cbd79058ea935.js","static/chunks/pages/index-07da1b317c79116e.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
